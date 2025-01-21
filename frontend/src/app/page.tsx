9336b5055f70f3b4743bb8d5316ef8cb34d285e0
Manali-Dama <manali.d@medkart.in>
2025-01-21 14:29:47 +0530

    Update page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,12 @@
-import Navbar from "@/components/Navbar";
-
+import Features from "@/components/Features";
+import Hero from "@/components/Hero";
+// import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
-  <>
-   <Navbar />
-
-  </>
+    <>
+    <Hero />
+    <Features />
+    <h1>Home</h1></>
   );
 }
-
